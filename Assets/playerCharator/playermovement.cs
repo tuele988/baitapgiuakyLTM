009ba4fed06967638cfda764ac3943c6338e287f
tuele988 <lvtue@tma.com.vn>
2025-11-05 01:04:20 +0700

    update

--- Assets/playerCharator/playermovement.cs
+++ Assets/playerCharator/playermovement.cs
@@ -1,19 +1,15 @@
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D body;
-    public float speed;
-
-    void Start()
-    {
-    }
+    public float speed = 5f;
 
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         Vector2 move = new Vector2(xInput, yInput);
-        body.linearVelocity = move*speed;
+        body.linearVelocity = move * speed;
     }
 }
